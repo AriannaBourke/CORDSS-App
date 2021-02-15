85feb3fece88315bf81c942e28bc676854adddfc
AriannaBourke <ariannabourke@hotmail.com>
2021-02-15 12:41:43 +0000

    camera

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { CameraOptions, Camera } from "@ionic-native/camera/ngx";
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,61 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  myProfileImage;
 
-  constructor() {}
+  constructor(
+    private _camera: Camera,
+    private _alertController: AlertController) {
+  }
 
+  async selectImageSource() {
+    const cameraOptions: CameraOptions = {
+        quality: 100,
+        destinationType: this._camera.DestinationType.DATA_URL,
+        encodingType: this._camera.EncodingType.JPEG,
+        mediaType: this._camera.MediaType.PICTURE,
+        targetHeight: 200,
+        correctOrientation: true,
+        sourceType: this._camera.PictureSourceType.CAMERA
+    };
+
+    const galleryOptions: CameraOptions = {
+      quality: 100,
+      destinationType: this._camera.DestinationType.DATA_URL,
+      encodingType: this._camera.EncodingType.JPEG,
+      mediaType: this._camera.MediaType.PICTURE,
+      targetHeight: 200,
+      correctOrientation: true,
+      sourceType: this._camera.PictureSourceType.SAVEDPHOTOALBUM
+  };
+    const alert = await this._alertController.create({
+      header: "Select Source",
+      message: "Pick a source for your avatar",
+      buttons: [
+        {
+          text:"Camera",
+          handler: ()=> {
+            this._camera.getPicture(cameraOptions)
+            .then((ImageData)=> {
+              this.myProfileImage = "data:image/jpeg;base64," + ImageData;
+            })
+
+          }
+        },
+        {
+          text:"Gallery",
+          handler: ()=> {
+            this._camera.getPicture(galleryOptions)
+            .then((ImageData)=> {
+              this.myProfileImage = "data:image/jpeg;base64," + ImageData;
+            })
+
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+
+  }
 }
