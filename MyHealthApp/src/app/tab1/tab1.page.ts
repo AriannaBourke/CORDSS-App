000dd14331842a7d42b2d3b7ca06e45c3fc87367
ElinaRotsidou <73070654+ElinaRotsidou@users.noreply.github.com>
2021-04-10 22:06:57 +0100

    call function

--- MyHealthApp/src/app/tab1/tab1.page.ts
+++ MyHealthApp/src/app/tab1/tab1.page.ts
@@ -214,7 +214,7 @@ export class Tab1Page {
           handler: ()=> {
             this._camera.getPicture(cameraOptions)
             .then((ImageData)=> {
-              this.myProfileImage = "data:image/jpeg;base64," + ImageData;
+              this.myProfileImage = ImageData;
             })
 
           }
--- MyHealthApp/src/app/tab5/tab5.page.ts
+++ MyHealthApp/src/app/tab5/tab5.page.ts
@@ -13,10 +13,10 @@ export class Tab5Page {
   constructor(private callNumber: CallNumber,
     ) {}
 
-  callNow(number) {
-    this.callNumber.callNumber(number, true)
-    .then(res => console.log('Launched dialer!', res))
-    .catch(err => console.log('Error launching dialer', err));
-  }
+    launchDialer(n:string){
+      this.callNumber.callNumber(n, true)
+      .then(() => console.log('Launched dialer!'))
+      .catch(() => console.log('Error launching dialer'));
+    }
 
 }
