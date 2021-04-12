fe5ce8898ed87a6f9f55130598cfcb3c41f0d66b
ElinaRotsidou <73070654+ElinaRotsidou@users.noreply.github.com>
2021-04-12 22:09:16 +0100

    update

--- MyHealthApp/src/app/app.component.ts
+++ MyHealthApp/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 
 
@@ -9,6 +12,33 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
-}
+
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ){
+    this.initializeApp();
+  }
+
+  initializeApp(){
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+
+    }
+  }
+
+  // constructor( private _plat: Platform, private splashScreen: SplashScreen) {
+    
+  //   this._plat.ready().then(() => {
+  //     setTimeout(() => {
+  //         this.splashScreen.hide();
+  
+  //     }, 800);
+  // });
+    
+  
+  // }}
 
--- MyHealthApp/src/app/app.module.ts
+++ MyHealthApp/src/app/app.module.ts
@@ -10,6 +10,9 @@ import { CallNumber } from '@ionic-native/call-number/ngx';
 import { SQLite } from '@ionic-native/sqlite/ngx';
 import { ImagePicker } from '@ionic-native/image-picker/ngx';
 import { File } from '@ionic-native/file/ngx'
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { FileOpener } from '@ionic-native/file-opener/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 
 
@@ -17,7 +20,7 @@ import { File } from '@ionic-native/file/ngx'
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule, ReactiveFormsModule],
-  providers: [ Camera, CallNumber, SQLite, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ImagePicker, File],
+  providers: [ Camera, CallNumber, SQLite, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ImagePicker, File, SplashScreen, FileOpener, StatusBar],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
