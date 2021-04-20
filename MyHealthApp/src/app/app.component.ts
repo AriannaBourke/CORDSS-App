50a27a470e837e67a0914edb2a0b7c625ca11c8b
Arianna Bourke <ariannabourke@hotmail.com>
2021-04-20 11:15:24 +0100

    update

--- MyHealthApp/src/app/app.component.ts
+++ MyHealthApp/src/app/app.component.ts
@@ -20,18 +20,7 @@ export class AppComponent {
   ){
     this.initializeApp();
   }
-
-  // initializeApp(){
-  //   this.platform.ready().then(() => {
-  //     this.statusBar.styleDefault();
-  //     this.splashScreen.hide();
-  //   });
-
-  //   }
-  // }
-
-
-  /// TRY THIS 
+  
   initializeApp(){
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
