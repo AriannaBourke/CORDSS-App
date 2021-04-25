8d4b751b8daf5374224d3f03ce9a18034bc375ff
Arianna Bourke <ariannabourke@hotmail.com>
2021-04-25 11:02:03 +0100

    console

--- CORDSS/src/app/tab3/test-results/edit-entry/edit-entry.page.ts
+++ CORDSS/src/app/tab3/test-results/edit-entry/edit-entry.page.ts
@@ -216,13 +216,11 @@ export class EditEntryPage {
         {
           text: 'No',
           handler: () => {
-            console.log('Disagree clicked');
           }
-        }, 
+        },
         {
           text: 'Yes',
           handler: () => {
-            console.log('Agree clicked');
             this.photos.splice(index, 1);
           }
         }
@@ -240,13 +238,11 @@ deleteAll(){
       {
         text: 'No',
         handler: () => {
-          console.log('Disagree clicked');
         }
-      }, 
+      },
       {
         text: 'Yes',
         handler: () => {
-          console.log('Agree clicked');
           this._db.executeSql('DELETE FROM pictures WHERE cardid=?', [this.rowid])
       .then(res => {
         this.getDataPictures(this.rowid);
--- CORDSS/src/app/tab3/urgent-health-plan/urgent-health-plan.page.ts
+++ CORDSS/src/app/tab3/urgent-health-plan/urgent-health-plan.page.ts
@@ -201,7 +201,6 @@ export class UrgentHealthPlanPage {
           });
         }
         if (this.aboutmepicture.length > 0) {
-          console.log(this.aboutmepicture[0].picture);
           this.myProfileImage = this.aboutmepicture[
             res.rows.length - 1
           ].picture;
